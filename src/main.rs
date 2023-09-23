05f8c379f84cf81848d8cc12a8da9965914e29ed
vaquero <froilan@virtual-artifact.com>
2023-09-23 11:02:15 +0100

    Write a tiny successful HTTP response to the stream

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,12 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
+    println!("Request: {:#?}", http_request);
 
-    println!("{:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    /*
+     The write_all method on stream takes a &[u8] and sends those bytes directly down the connection.
+     */
+    stream.write_all(response.as_bytes()).unwrap();
 }
