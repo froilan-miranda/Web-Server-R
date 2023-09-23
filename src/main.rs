a9170f2795f0ccf9eb926261f659f7661f2b20f5
vaquero <froilan@virtual-artifact.com>
2023-09-23 11:27:28 +0100

    Respond with status code 404 and an error page if anything other than / was requested

--- src/main.rs
+++ src/main.rs
@@ -37,22 +37,25 @@ fn main() {
 */
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: {:#?}", http_request);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+        let response =
+            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        /*
+        The write_all method on stream takes a &[u8] 
+        and sends those bytes directly down the connection.
+        */
+        stream.write_all(response.as_bytes()).unwrap();    
+    } else {
+        // handle other requests
+    }
 
-    /*
-     The write_all method on stream takes a &[u8] and sends those bytes directly down the connection.
-     */
-    stream.write_all(response.as_bytes()).unwrap();
+
+    
 }
