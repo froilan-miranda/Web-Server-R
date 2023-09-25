1faea046040ee81be2e2942a0f40767556373e1c
vaquero <froilan@virtual-artifact.com>
2023-09-25 12:18:04 +0100

    implement the simplest new function

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
 pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        ThreadPool
+    }
+}
+
+/*
+We chose usize as the type of the size parameter, because we know that a negative number of threads doesn’t make any sense. We also know we’ll use this 4 as the number of elements in a collection of threads, which is what the usize type is for
+*/
